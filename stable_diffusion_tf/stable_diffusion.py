503a79b6b177e23d0d8b0549631fbfa575ba9f08
David Baynard <git@baynard.dev>
2022-10-25 23:45:45 +0100

    Use weights from nix store

--- stable_diffusion_tf/stable_diffusion.py
+++ stable_diffusion_tf/stable_diffusion.py
@@ -224,20 +224,24 @@ def get_models(img_height, img_width, download_weights=True):
 
     if download_weights:
         text_encoder_weights_fpath = keras.utils.get_file(
-            origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/text_encoder.h5",
+            origin="file:///nix/store/l9j54fr834qg2mpvlkjm4jnzr24a5w1j-stable-diffusion-weights/text_encoder_weights_fpath",
+            fname="/nix/store/l9j54fr834qg2mpvlkjm4jnzr24a5w1j-stable-diffusion-weights/text_encoder_weights_fpath",
             file_hash="d7805118aeb156fc1d39e38a9a082b05501e2af8c8fbdc1753c9cb85212d6619",
         )
         diffusion_model_weights_fpath = keras.utils.get_file(
-            origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/diffusion_model.h5",
+            origin="file:///nix/store/l9j54fr834qg2mpvlkjm4jnzr24a5w1j-stable-diffusion-weights/diffusion_model_weights_fpath",
+            fname="/nix/store/l9j54fr834qg2mpvlkjm4jnzr24a5w1j-stable-diffusion-weights/diffusion_model_weights_fpath",
             file_hash="a5b2eea58365b18b40caee689a2e5d00f4c31dbcb4e1d58a9cf1071f55bbbd3a",
         )
         decoder_weights_fpath = keras.utils.get_file(
-            origin="https://huggingface.co/fchollet/stable-diffusion/resolve/main/decoder.h5",
+            origin="file:///nix/store/l9j54fr834qg2mpvlkjm4jnzr24a5w1j-stable-diffusion-weights/decoder_weights_fpath",
+            fname="/nix/store/l9j54fr834qg2mpvlkjm4jnzr24a5w1j-stable-diffusion-weights/decoder_weights_fpath",
             file_hash="6d3c5ba91d5cc2b134da881aaa157b2d2adc648e5625560e3ed199561d0e39d5",
         )
 
         encoder_weights_fpath = keras.utils.get_file(
-            origin="https://huggingface.co/divamgupta/stable-diffusion-tensorflow/resolve/main/encoder_newW.h5",
+            origin="file:///nix/store/l9j54fr834qg2mpvlkjm4jnzr24a5w1j-stable-diffusion-weights/encoder_weights_fpath",
+            fname="/nix/store/l9j54fr834qg2mpvlkjm4jnzr24a5w1j-stable-diffusion-weights/encoder_weights_fpath",
             file_hash="56a2578423c640746c5e90c0a789b9b11481f47497f817e65b44a1a5538af754",
         )
 
