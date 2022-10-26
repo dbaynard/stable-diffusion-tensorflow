e325b1316a0da8e0b66a595dc5dc48075bc90a63
David Baynard <git@baynard.dev>
2022-10-26 21:10:50 +0100

    Add python dependency overrides

--- default.nix
+++ default.nix
@@ -3,8 +3,25 @@
 , poetry2nix
 }:
 
+let
+  withSetuptools = pnames: final: prev:
+    let
+      f = pname: prev.${pname}.overrideAttrs (old: {
+        buildInputs = old.buildInputs or [ ] ++ [ prev.setuptools ];
+      });
+    in
+    lib.listToAttrs (map (name: { inherit name; value = f name; }) pnames);
+
+in
+
 poetry2nix.mkPoetryApplication {
   projectDir = ./.;
   propagatedBuildInputs = [ diffusion-models ];
   dontUseWheelUnpack = true;
+
+  overrides = poetry2nix.overrides.withDefaults (withSetuptools [
+    "libclang"
+    "pyparsing"
+    "typing-extensions"
+  ]);
 }
