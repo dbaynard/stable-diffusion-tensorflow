9cfa8085b764df860ce67f2cfa59265bf450f46d
David Baynard <git@baynard.dev>
2022-11-04 02:30:31 +0000

    Handle img2img in create.py

--- stable_diffusion_tf/create.py
+++ stable_diffusion_tf/create.py
@@ -14,11 +14,29 @@ class Text2Img(NamedTuple):
         return cls(**{**run, "output": Path(run['output'])})
 
 
-Run = Text2Img
+class Img2Img(NamedTuple):
+    """A single run of stable diffusion img2img"""
+
+    input: Path
+    prompt: str
+    output: Path
+    seed: int = 42
+
+    @classmethod
+    def from_json(cls, run: str):
+        return cls(
+            **{**run, "input": Path(run['input']), "output": Path(run['output'])}
+        )
+
+
+Run = Text2Img | Img2Img
 
 
 def from_json(run: str):
-    return Text2Img.from_json(run)
+    try:
+        return Text2Img.from_json(run)
+    except (TypeError, KeyError):
+        return Img2Img.from_json(run)
 
 
 class Args(NamedTuple):
@@ -36,6 +54,7 @@ def create(runs: list[Run], args=Args()):
     generator = StableDiffusion(img_height=args.H, img_width=args.W, jit_compile=False)
 
     for run in runs:
+        input_image = f"{run.input.expanduser()}" if type(run) == Img2Img else None
         img = generator.generate(
             run.prompt,
             num_steps=args.steps,
@@ -43,6 +62,8 @@ def create(runs: list[Run], args=Args()):
             temperature=1,
             batch_size=1,
             seed=run.seed,
+            input_image=input_image,
+            input_image_strength=0.8,
         )
         Image.fromarray(img[0]).save(run.output)
         print(f"saved at {run.output}")
